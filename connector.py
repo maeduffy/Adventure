dde60da3bd37adbde07dd4c20914f71d7997cf9a
Cara Pew <cpew@calpoly.edu>
2016-06-08 23:56:31 -0700

    Fixed connector errors

--- connector.py
+++ connector.py
@@ -12,43 +12,33 @@ db = MySQLdb.connect(host = "csc-db0", user = "cpew", passwd = "FunToPassHa7", d
 cur = db.cursor()
 
 def addCharacter(name):
-	insert = "INSERT INTO Characters VALUES (NULL, '%s', 100, 1);" % name
-	cur.execute(insert)
+	cur.execute("INSERT INTO Characters VALUES (NULL, %s, 100, 1)", (name))
 	db.commit()
 
 def charExists(name):
-	select = "SELECT COUNT(*) FROM (SELECT name FROM Characters WHERE name = '%s') AS names;" % name
-	cur.execute(select)
+	cur.execute("SELECT COUNT(*) FROM (SELECT name FROM Characters WHERE name = %s) AS names", (name))
 	return cur.fetchall
 
 def roomText(id):
-	texts = "SELECT id, phrase FROM Rooms WHERE id = (SELECT pass1 FROM Rooms WHERE id = %d) OR id = (SELECT pass2 FROM Rooms WHERE id = %d) OR id = (SELECT pass3 FROM Rooms WHERE id = %d);" % (id, id, id)
-	cur.execute(texts)
-	return cur.fetchall
+	cur.execute("SELECT id, phrase FROM Rooms WHERE id = (SELECT pass1 FROM Rooms WHERE id = %d) OR id = (SELECT pass2 FROM Rooms WHERE id = %d) OR id = (SELECT pass3 FROM Rooms WHERE id = %d)", (id, id, id))
+	return cur.fetchall()
 
 def addCurrentItem(char, item):
-	insert = "INSERT INTO CurrentItems VALUES ('%s', %d);" % (char, item)
-	cur.execute(insert)
+	cur.execute("INSERT INTO CurrentItems VALUES (%s, %d)", (char, item))
 	db.commit()
 
 def checkProjectComplete(char, project):
-	projects = "SELECT COUNT(*) FROM CompletedProjects WHERE CharId = '%s' AND id = %d;" % (char, project)
-	cur.execute(projects)
-	return cur.fetchall
+	cur.execute("SELECT COUNT(*) FROM CompletedProjects WHERE CharId = %s AND id = %d", (char, project))
+	return cur.fetchall()
 
 def setProjectComplete(char, project, score):
-	insert = "INSERT INTO CompletedProjects VALUES('%s', %d, %d);" % (char, project, score);
-	cur.execute(insert)
+	cur.execute("INSERT INTO CompletedProjects VALUES(%s, %d, %d)", (char, project, score))
 	db.commit()
 
 def checkTestComplete(char, test):
-	tests = "SELECT COUNT(*) FROM CompletedProjects WHERE CharId = '%s' AND id = %d;" % (char, test)
-	cur.execute(tests)
-	return cur.fetchall
+	cur.execute("SELECT COUNT(*) FROM CompletedProjects WHERE CharId = %s AND id = %d", (char, test))
+	return cur.fetchall()
 
 def setTestComplete(char, test, score):
-	insert = "INSERT INTO CompletedTests VALUES('%s', %d, %d);" % (char, test, score);
-	cur.execute(insert)
+	cur.execute("INSERT INTO CompletedTests VALUES(%s, %d, %d)", (char, test, score))
 	db.commit()
-
-
